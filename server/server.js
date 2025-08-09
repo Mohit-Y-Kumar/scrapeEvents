93aa61186e7f4275e5724392e0ecbdbbc8546451
Mohit-Y-Kumar <my1362321@gmail.com>
2025-08-10 00:14:19 +0530

    fix server

--- server/server.js
+++ server/server.js
@@ -24,7 +24,7 @@ mongoose.connect(process.env.MONGO_URL, {
 });
 
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: 'https://scrapeevents-2.onrender.com',
   credentials: true, 
 }));
 app.use(express.json());
